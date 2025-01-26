230adb939f07d42c1b2c1fc00f0d67d5c3cc6f5c
ViLelouch <sepidermn6@gmail.com>
2025-01-27 06:38:20 +0700

    test build

--- leaf.sh
+++ leaf.sh
@@ -9,13 +9,13 @@ rm -rf kernel/xiaomi/mojito
 rm -rf vendor/xiaomi/mojito
 
 # Initialize ROM manifest
-repo init -u https://github.com/LeafOS-Project/android.git -b leaf-3.2
+repo init -u https://github.com/The-Pixel-Project/manifest -b fourteen-qpr3 -g default,-mips,-darwin,-notdefault --git-lfs
 
 # Sync the repo with force to ensure a clean sync
 /opt/crave/resync.sh
 
 # cloning device tree
-git clone https://github.com/ViLelouch/android_device_xiaomi_mojito.git --depth 1 -b main device/xiaomi/mojito
+git clone https://github.com/ViLelouch/android_device_xiaomi_mojito.git --depth 1 -b aosp device/xiaomi/mojito
 
 # cloning kernel tree
 git clone https://github.com/Kou-Yeager/kernel_xiaomi_mojito.git --depth 1 -b BORE+UCLAMP kernel/xiaomi/mojito
@@ -23,14 +23,11 @@ git clone https://github.com/Kou-Yeager/kernel_xiaomi_mojito.git --depth 1 -b BO
 # cloning vendor tree
 git clone https://github.com/Kou-Yeager/android_vendor_xiaomi_mojito.git --depth 1 -b lineage-22 vendor/xiaomi/mojito
 
-# Export
-export WITH_GMS=true
-
 # Set up th build environment
 . build/envsetup.sh
 
 # Choose the target device
-lunch leaf_mojito-ap2a-userdebug
+lunch aosp_mojito-userdebug
 
 # Build the ROM (use mka bacon for a full build)
-m leaf
+make bacon
